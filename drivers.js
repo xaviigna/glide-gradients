cb7904334d7f054fedbf081a4151719551d45f65
Xavier Benavides <xavierbenavidesm@gmail.com>
2026-01-20 19:27:54 -0600

    Restore generic drivers.js to fix infinite loading

---
+++ drivers.js
@@ -0,0 +1,26 @@
+window.addEventListener("message", async function(event) {
+    const { origin, data: { key, params } } = event;
+  
+    let result;
+    let error;
+    try {
+      result = await window.function(...params);
+    } catch (e) {
+      result = undefined;
+      try {
+        error = e.toString();
+      } catch (e) {
+        error = "Exception can't be stringified.";
+      }
+    }
+  
+    const response = { key };
+    if (result !== undefined) {
+      response.result = { value: result };
+    }
+    if (error !== undefined) {
+      response.error = error;
+    }
+  
+    event.source.postMessage(response, "*");
+});
